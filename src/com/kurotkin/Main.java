3345d495b871a1d3a468b51a6199f81b62b6604b
kurotkin <kurotkin@gmail.com>
2017-04-24 14:50:41 +0300

    test Postgresql

--- src/com/kurotkin/Main.java
+++ src/com/kurotkin/Main.java
@@ -20,17 +20,7 @@ public class Main {
         //String jsonLine = "{\"data\": { \"translations\": [ { \"translatedText\": \"Hello world\"}]}} ";
 
         String jsonLine = TestVals.getJSON();
-        //JsonElement jelement = new JsonParser().parse(jsonLine);
-        //JsonObject jobject = jelement.getAsJsonObject();
-        //jobject = jobject.getAsJsonObject("data");
-        //JsonArray jarray = jobject.getAsJsonArray("translations");
-        //jobject = jarray.get(0).getAsJsonObject();
-        //String result = jobject.get("translatedText").toString();
-        //System.out.println(result);
-        //Map jsonJavaRootObject = new Gson().fromJson(jsonString, Map.class);
-        //System.out.println(jsonJavaRootObject);
 
-        String s = "[{\"number\" : \"3\",\"title\" : \"hello_world\"},{\"number\" : \"2\",\"title\" : \"hello_world\"}]";
         Gson gson = new Gson();
         Device[] data = gson.fromJson(jsonLine, Device[].class);
         for(Device d : data) {
@@ -42,6 +32,11 @@ public class Main {
         }
         System.out.println(data[0].name);
 
+        PostgesqlConn.createTable2();
+        //PostgesqlConn.insert();
+        PostgesqlConn.select();
+        PostgesqlConn.update();
+        PostgesqlConn.del();
     }
 
 }
--- src/com/kurotkin/PostgesqlConn.java
+++ src/com/kurotkin/PostgesqlConn.java
@@ -1,5 +1,7 @@
 package com.kurotkin;
 
+import com.sun.org.apache.xpath.internal.functions.FuncFalse;
+
 import java.sql.*;
 
 /**
@@ -23,13 +25,130 @@ public class PostgesqlConn {
 
     public static void createTable () throws SQLException {
         Connection conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/demo", "postgres", "33321");
-        PreparedStatement ps = conn.prepareStatement("CREATE TABLE IF NOT EXISTS vals(" +
+        PreparedStatement ps = conn.prepareStatement("CREATE TABLE IF NOT EXISTS vals(" +   // CREATE TABLE IF NOT EXISTS vals
                                                                                         "CREATED_DATE NOT NULL PRIMARY KEY," +
                                                                                         "username varchar(225) NOT NULL UNIQUE," +
                                                                                         "password varchar(225)," +
                                                                                         "islogged varchar(10))");
         ps.executeUpdate();
         ps.close();
+        conn.close();
+    }
+
+    public static void createTable2 () throws SQLException {
+        Connection conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/demo", "postgres", "33321");
+        Statement stmt = conn.createStatement();
+        String sql = "CREATE TABLE IF NOT EXISTS COMPANY " +
+                "(ID INT PRIMARY KEY     NOT NULL," +
+                " NAME           TEXT    NOT NULL, " +
+                " AGE            INT     NOT NULL, " +
+                " ADDRESS        CHAR(50), " +
+                " SALARY         REAL)";
+        stmt.executeUpdate(sql);
+        stmt.close();
+        conn.close();
+    }
+
+    public static void insert () throws SQLException {
+        Connection conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/demo", "postgres", "33321");
+        conn.setAutoCommit(false);
+        Statement stmt = conn.createStatement();
+        String sql = "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) "
+                + "VALUES (1, 'Paul', 32, 'California', 20000.00 );";
+        stmt.executeUpdate(sql);
+
+        sql = "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) "
+                + "VALUES (2, 'Allen', 25, 'Texas', 15000.00 );";
+        stmt.executeUpdate(sql);
+
+        sql = "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) "
+                + "VALUES (3, 'Teddy', 23, 'Norway', 20000.00 );";
+        stmt.executeUpdate(sql);
+
+        sql = "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) "
+                + "VALUES (4, 'Mark', 25, 'Rich-Mond ', 65000.00 );";
+        stmt.executeUpdate(sql);
+
+        stmt.close();
+        conn.commit();
+        conn.close();
+    }
+
+    public static void select () throws SQLException {
+        Connection conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/demo", "postgres", "33321");
+        conn.setAutoCommit(false);
+        Statement stmt = conn.createStatement();
+        ResultSet rs = stmt.executeQuery( "SELECT * FROM COMPANY;" );
+        while ( rs.next() ) {
+            int id = rs.getInt("id");
+            String  name = rs.getString("name");
+            int age  = rs.getInt("age");
+            String  address = rs.getString("address");
+            float salary = rs.getFloat("salary");
+            System.out.println( "ID = " + id );
+            System.out.println( "NAME = " + name );
+            System.out.println( "AGE = " + age );
+            System.out.println( "ADDRESS = " + address );
+            System.out.println( "SALARY = " + salary );
+            System.out.println();
+        }
+        rs.close();
+        stmt.close();
+        conn.close();
+    }
+
+    public static void update () throws SQLException {
+        Connection conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/demo", "postgres", "33321");
+        conn.setAutoCommit(false);
+        Statement stmt = conn.createStatement();
+        String sql = "UPDATE COMPANY set SALARY = 25000.00 where ID=1;";
+        stmt.executeUpdate(sql);
+        conn.commit();
+
+        ResultSet rs = stmt.executeQuery( "SELECT * FROM COMPANY;" );
+        while ( rs.next() ) {
+            int id = rs.getInt("id");
+            String  name = rs.getString("name");
+            int age  = rs.getInt("age");
+            String  address = rs.getString("address");
+            float salary = rs.getFloat("salary");
+            System.out.println( "ID = " + id );
+            System.out.println( "NAME = " + name );
+            System.out.println( "AGE = " + age );
+            System.out.println( "ADDRESS = " + address );
+            System.out.println( "SALARY = " + salary );
+            System.out.println();
+        }
+        rs.close();
+        stmt.close();
+        conn.close();
+    }
+
+    public static void del () throws SQLException {
+        Connection conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/demo", "postgres", "33321");
+        conn.setAutoCommit(false);
+        Statement stmt = conn.createStatement();
+
+        String sql = "DELETE from COMPANY where ID = 2;";
+        stmt.executeUpdate(sql);
+        conn.commit();
+
+        ResultSet rs = stmt.executeQuery( "SELECT * FROM COMPANY;" );
+        while ( rs.next() ) {
+            int id = rs.getInt("id");
+            String  name = rs.getString("name");
+            int age  = rs.getInt("age");
+            String  address = rs.getString("address");
+            float salary = rs.getFloat("salary");
+            System.out.println( "ID = " + id );
+            System.out.println( "NAME = " + name );
+            System.out.println( "AGE = " + age );
+            System.out.println( "ADDRESS = " + address );
+            System.out.println( "SALARY = " + salary );
+            System.out.println();
+        }
+        rs.close();
+        stmt.close();
         conn.close();
     }
 
