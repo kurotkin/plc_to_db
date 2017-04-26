b473ad54d51dd43657d22054d0cbfaa5e6c5103c
kurotkin <kurotkin@gmail.com>
2017-04-26 16:57:26 +0300

    test ExecutorService

--- src/com/kurotkin/Main.java
+++ src/com/kurotkin/Main.java
@@ -1,22 +1,28 @@
 package com.kurotkin;
 import com.google.gson.*;
 import java.sql.SQLException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
 
     public static void main(String[] args) throws SQLException {
+        ScheduledExecutorService service = Executors.newSingleThreadScheduledExecutor();
+        service.scheduleAtFixedRate(new Runnable() {
+            // Получение данных
+            HTTPgetter getJson = new HTTPgetter("http://192.168.1.50/awp/json.htm");
+            //String jsonLine = getJson.get();
+            String jsonLine = TestVals.getJSON();
 
-        // Получение данных
-        HTTPgetter getJson = new HTTPgetter("http://192.168.1.50/awp/json.htm");
-        //String jsonLine = getJson.get();
-        String jsonLine = TestVals.getJSON();
+            // Парсинг
+            Gson gson = new Gson();
+            Device[] data = gson.fromJson(jsonLine, Device[].class);
 
-        // Парсинг
-        Gson gson = new Gson();
-        Device[] data = gson.fromJson(jsonLine, Device[].class);
+            // Запирсь в БД
+            PostgesqlConn.insert2(data);
 
-        // Запирсь в БД
-        PostgesqlConn.insert2(data);
+        }, 0, 5, TimeUnit.SECONDS);
 
     }
 
